2a5750cb5b7cb2c54d3ecf2d48a7edaf2bd9a297
Gökay Şatır <gokaysatir@collabora.com>
2025-03-06 13:09:00 +0300

    Remove _tileWidthPx and _tileHeightPx and use TileManager.tileSize.
    Signed-off-by: Gökay Şatır <gokaysatir@collabora.com>
    Change-Id: Ieaea97b4d527d3ea850e59af08d68eab633eab7b

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -611,9 +611,6 @@ L.CanvasTileLayer = L.Layer.extend({
 	initialize: function (options) {
 		options = L.setOptions(this, options);
 
-		this._tileWidthPx = options.tileSize;
-		this._tileHeightPx = options.tileSize;
-
 		// text, presentation, spreadsheet, etc
 		this._docType = options.docType;
 		this._documentInfo = '';
@@ -918,8 +915,8 @@ L.CanvasTileLayer = L.Layer.extend({
 		if (!this._map._docLoaded)
 			return;
 
-		var newClientZoom = 'tilepixelwidth=' + this._tileWidthPx + ' ' +
-		    'tilepixelheight=' + this._tileHeightPx + ' ' +
+		var newClientZoom = 'tilepixelwidth=' + TileManager.tileSize + ' ' +
+		    'tilepixelheight=' + TileManager.tileSize + ' ' +
 		    'tiletwipwidth=' + app.tile.size.x + ' ' +
 		    'tiletwipheight=' + app.tile.size.y + ' ' +
 		    'dpiscale=' + window.devicePixelRatio + ' ' +
@@ -3435,8 +3432,8 @@ L.CanvasTileLayer = L.Layer.extend({
 
 	requestCellCursor: function() {
 		app.socket.sendMessage('commandvalues command=.uno:CellCursor'
-			+ '?outputHeight=' + this._tileWidthPx
-			+ '&outputWidth=' + this._tileHeightPx
+			+ '?outputHeight=' + TileManager.tileSize
+			+ '&outputWidth=' + TileManager.tileSize
 			+ '&tileHeight=' + app.tile.size.x
 			+ '&tileWidth=' + app.tile.size.y);
 	},
@@ -3883,9 +3880,6 @@ L.CanvasTileLayer = L.Layer.extend({
 	},
 
 	onAdd: function (map) {
-		this._tileWidthPx = this.options.tileSize;
-		this._tileHeightPx = this.options.tileSize;
-
 		this._initContainer();
 
 		// Initiate selection handles.
